e30edd360a5975c35304250e227a4eadbed53e17
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-07-23 14:03:44 +0200

    chore(chore): get git history

--- workshop/apps/08-loop/src/views/router.js
+++
@@ -1,9 +0,0 @@
-import VueRouter from 'vue-router';
-import Home from './Home.vue';
-
-const APP_ROUTES = [
-  { path: '', redirect: { name: 'home' } },
-  { path: '/home', component: Home, name: 'home' },
-];
-
-export default new VueRouter({ routes: APP_ROUTES });
--- workshop/apps/19-state-management-module-solution/src/store/person/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-  setPeople(state, people) {
-    state.people = people;
-  },
-  setSearch(state, search = '') {
-    state.search = search;
-  },
-};
