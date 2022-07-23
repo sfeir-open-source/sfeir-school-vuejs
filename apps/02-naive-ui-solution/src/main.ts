93a1c7f268a3019cc1fb78857ca3de5400be9df1
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-07-23 13:32:23 +0200

    feat(workshop): :sparkles: add naive ui integration workshop

---
+++ apps/02-naive-ui-solution/src/main.ts
@@ -0,0 +1,14 @@
+import naive from 'naive-ui';
+import { createApp } from 'vue';
+import './assets/main.css';
+
+createApp({
+  el: '#app',
+  data() {
+    return {
+      name: 'SFEIR'
+    };
+  }
+})
+  .use(naive)
+  .mount('#app');
---
+++ apps/02-naive-ui/src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import './assets/main.css';
+
+createApp({
+  el: '#app',
+  data() {
+    return {
+      name: 'SFEIR'
+    };
+  }
+}).mount('#app');
