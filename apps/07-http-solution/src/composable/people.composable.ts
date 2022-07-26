24c3d0de52d00b79c2f8735ebc77c9b76c658bc4
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-07-26 14:44:49 +0200

    feat(workshop): :sparkles: add http request workshop

---
+++ apps/07-http-solution/src/composable/people.composable.ts
@@ -0,0 +1,24 @@
+import type { Person } from '@/models/person.model';
+import axios from 'axios';
+import { ref, type Ref } from 'vue';
+
+const people: Ref<Array<Person>> = ref([]);
+
+export function usePeople() {
+  const getPeople: () => Promise<void> = async () => {
+    if (people.value.length === 0) {
+      people.value = (await axios.get<Array<Person>>(`${import.meta.env.VITE_BASE_API}/peoples`)).data;
+    }
+  };
+
+  const getRandomPeople: () => Promise<Person> = async () => {
+    const { data } = await axios.get<Person>(`${import.meta.env.VITE_BASE_API}/peoples/random`);
+    return data;
+  };
+
+  return {
+    people,
+    getPeople,
+    getRandomPeople
+  };
+}
