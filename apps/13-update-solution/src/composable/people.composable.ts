cf69cda135228200051d2313e80436b52e483b43
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-07-29 19:22:50 +0200

    feat(workshop): :sparkles: add 13-update workshop

---
+++ apps/13-update-solution/src/composable/people.composable.ts
@@ -0,0 +1,44 @@
+import type { Person, PersonForm } from '@/models/person.model';
+import axios from 'axios';
+import { ref, type Ref } from 'vue';
+
+const people: Ref<Array<Person>> = ref([]);
+
+export function usePeople() {
+  const getPeople: () => Promise<void> = async () => {
+    people.value = (await axios.get<Array<Person>>(`${import.meta.env.VITE_BASE_API}/peoples`)).data;
+  };
+
+  const getRandomPeople: () => Promise<Person> = async () => {
+    const { data } = await axios.get<Person>(`${import.meta.env.VITE_BASE_API}/peoples/random`);
+    return data;
+  };
+
+  const deletePerson: (id: string) => Promise<void> = async (idPerson: string) => {
+    const { data } = await axios.delete<Array<Person>>(`${import.meta.env.VITE_BASE_API}/peoples/${idPerson}`);
+    people.value = data;
+  };
+
+  const addPerson: (personForm: PersonForm) => Promise<void> = async (personForm: PersonForm) => {
+    await axios.post<Person>(`${import.meta.env.VITE_BASE_API}/peoples`, personForm);
+  };
+
+  const getPersonById: (id: string) => Promise<Person> = async (idPerson: string) => {
+    const { data } = await axios.get<Person>(`${import.meta.env.VITE_BASE_API}/peoples/${idPerson} `);
+    return data;
+  };
+
+  const updatePersonById: (personForm: PersonForm) => Promise<void> = async (personForm: PersonForm) => {
+    await axios.put<Person>(`${import.meta.env.VITE_BASE_API}/peoples/${personForm.id}`, personForm);
+  };
+
+  return {
+    people,
+    getPeople,
+    getRandomPeople,
+    deletePerson,
+    addPerson,
+    getPersonById,
+    updatePersonById
+  };
+}
---
+++ apps/13-update/src/views/Update.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container"></section>
+</template>
+
+<script lang="ts" async setup></script>
+
+<style lang="css" scoped>
+.container {
+  padding: 0 2rem;
+}
+</style>
